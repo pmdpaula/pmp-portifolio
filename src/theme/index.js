01eeec1280e0ca9d6b28081015f9121c751450c6
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-02-16 23:12:13 -0300

    Configuração inicial com tema

---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { typographyVariants } from './typographyVariants'
+
+export const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200
+}
+
+export const defaultStyles = {
+  borderRadius: '6px',
+  transition: '200ms ease-in-out',
+  fontFamily: "'Roboto', sans-serif",
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)'
+}
+
---
+++ src/theme/light.js
@@ -0,0 +1,59 @@
+import { typographyVariants } from './typographyVariants'
+import { breakpoints, defaultStyles } from './index'
+
+export const colors = {
+  background: {
+    main: {
+      color: '#cfd8dc'
+    },
+    secondary: {
+      color: '#eceff1'
+    }
+  },
+  text: {
+    head1: {
+      color: '#000a12'
+    },
+    head2: {
+      color: '#263238'
+    },
+    main: {
+      color: '#29434E'
+    }
+  },
+  borders: {
+    main: {
+      color: '#ffad42'
+    }
+  },
+  primary: {
+    main: {
+      color: '#546E7A',
+      contrastText: '#fff'
+    }
+  },
+  secondary: {
+    main: {
+      color: '#F57C00',
+      contrastText: '#29434E'
+    }
+  },
+  tertiary: {
+    main: {
+      color: '#070C0E',
+      contrastText: '#fff'
+    },
+    light: {
+      color: '#819CA9',
+      contrastText: '#fff'
+    }
+  }
+}
+
+export default {
+  title: 'light',
+  colors,
+  typographyVariants,
+  breakpoints,
+  defaultStyles
+}
