84da42415dc118bff8ea542c17c38d4bf81ea85c
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-02-18 18:52:08 -0300

    Prettier

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
-import { ThemeProvider } from 'styled-components'
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 
-import Head from 'next/head'
+import Head from 'next/head';
 
-import GlobalStyle from '../theme/GlobalStyle'
-import ThemeLight from '../theme/light'
-import ThemeDark from '../theme/dark'
+import GlobalStyle from '../theme/GlobalStyle';
+import ThemeLight from '../theme/light';
+import ThemeDark from '../theme/dark';
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState(ThemeDark)
+  const [theme, setTheme] = useState(ThemeDark);
 
   return (
     <>
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE_PAGE}</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
