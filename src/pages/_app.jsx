1fc55b27214dc3d72f64970a1f9f63435a3bd7b5
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-03-06 19:40:09 -0300

    docs(metatag): metatags para SEO
    Adição de diversas metatags para exibição em SEO

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,6 +16,32 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE_PAGE}</title>
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://portifolio-pmdpaula.vercel.app/"
+        />
+        <meta property="og:title" content="Portifólio de Pedro de Paula" />
+        <meta
+          property="og:description"
+          content="Portifólio de projetos em programação de Pedro Machado de Paula"
+        />
+        <meta property="og:image" content="" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://portifolio-pmdpaula.vercel.app/"
+        />
+        <meta property="twitter:title" content="Portifólio de Pedro de Paula" />
+        <meta
+          property="twitter:description"
+          content="Portifólio de projetos em programação de Pedro Machado de Paula"
+        />
+        <meta property="twitter:image" content="/images/seo.jpg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap"
