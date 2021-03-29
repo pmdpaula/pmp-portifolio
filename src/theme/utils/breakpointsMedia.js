dee4308a013bc875258537d7f66b9c6a2d749ba3
Pedro de Paula <pmdpaula@yahoo.com.br>
2021-03-29 13:00:01 -0300

    Update breakpointsMedia.js
    Ajuste no sort dos imports

--- src/theme/utils/breakpointsMedia.js
+++ src/theme/utils/breakpointsMedia.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-import { PropTypes } from 'prop-types';
 import { css } from 'styled-components';
+import { PropTypes } from 'prop-types';
 import { breakpoints } from '../index';
 
 const breakpointsMedia = cssByBreakpoint => {
